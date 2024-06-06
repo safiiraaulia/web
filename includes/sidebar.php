512629d4734c1b0d9ef22d2924bf730ea7b47f93
widyastibella <wdyabella@gmail.com>
2024-06-06 21:20:48 +0700

    contoh css dokter

--- includes/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="list-group">
-    <a href="index.php" class="list-group-item list-group-item-action">Dashboard</a>
-    <a href="views/daftar_pengguna.php" class="list-group-item list-group-item-action">Pengguna</a>
-    <a href="views/daftar_pasien.php" class="list-group-item list-group-item-action">Pasien</a>
-    <a href="views/daftar_dokter.php" class="list-group-item list-group-item-action">Dokter</a>
-    <a href="views/daftar_janjitemu.php" class="list-group-item list-group-item-action">Janji Temu</a>
-    <a href="views/daftar_inventaris.php" class="list-group-item list-group-item-action">Inventaris</a>
-
-</div>
